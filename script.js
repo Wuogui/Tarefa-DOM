605a5c17dcbf513961c2be320f56968ac4278f03
Wuogui <133606772+Wuogui@users.noreply.github.com>
2025-07-25 13:40:08 -0300

    feat(script): mensagens de erro

--- script.js
+++ script.js
@@ -8,8 +8,17 @@ let calculo;
 //função que adicionar as notas no bloco de texto
 function adicionar(numeroDaNota){
     n = nota.value.replace(",", ".");
+    if (Number.isNaN(nota.value)  || nota.value == "") {
+        alert("Por favor, insira uma nota")
+        return
+    }
     nota.value = "";
+    if (Number.isNaN(parseFloat(n)) || parseFloat(n) > 10 || parseFloat(n) < 0) {
+        alert("A nota digitada é inválida, por favor, insira uma nota válida")
+        return
+    }
     caixa.innerText += `\n A nota ${numeroDaNota} foi ${n}`;
+    n = parseFloat(n)
     i += 1;
     listaDeNotas.push(parseFloat(n));
 }
