926f2f2b0f0c279ec9a2a1fd6f051e0df2152835
Wuogui <133606772+Wuogui@users.noreply.github.com>
2025-07-25 13:18:04 -0300

    feat(script): calcular media concluido
    Parte que falta: detecção de erros

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
 let caixa = document.getElementById("caixa");
 let nota = document.querySelector("#entrada");
-let i = 1;
-
-//função que "trata" a nota para que ela seja legivel pelo programa, colocando pontos no lugar de vírgulas.
-function tratamento(){
-}
+let resultado = document.querySelector("#resultado")
+let i = 1; // Valor que dita qual o numero da nota está sendo inserida.
+let listaDeNotas = [] //lista de notas as notas inseridas pelo usuário.
+let calculo;
 
 //função que adicionar as notas no bloco de texto
 function adicionar(numeroDaNota){
-    caixa.innerText += `\n A nota ${numeroDaNota} foi ${nota.value}`;
-    nota.value = ""
+    n = nota.value.replace(",", ".");
+    nota.value = "";
+    caixa.innerText += `\n A nota ${numeroDaNota} foi ${n}`;
     i += 1;
+    listaDeNotas.push(parseFloat(n));
 }
 
 //função que limpa todo o bloco de texto
 function limpar(){
     caixa.innerText = ''
     i = 1
+    listaDeNotas = []
+    resultado.innerText = ""
 }
 
 //função que calcula a média das notas
 function media(){
+    for (let k=0; k < listaDeNotas.length; k++){
+        if (k == 0) {
+            calculo = listaDeNotas[k]
+        }
+        else {
+            calculo += listaDeNotas[k]
+        }
+    }
+    calculo = calculo / listaDeNotas.length
+    resultado.innerText = `${calculo.toFixed(2)}`
 }
 
 let btnEnviar = document.querySelector("#btnEnviar");
